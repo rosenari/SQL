925ed38f7649ecb400d8cf24e63bbd335c580acb
임장순 <49670068+rosenari@users.noreply.github.com>
2021-11-04 22:07:59 +0900

    Update and rename SELECT.md to SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -2,36 +2,36 @@
 
 SELECT * FROM ANIMAL_INS ORDER BY ANIMAL_ID;
 
-### 역순 정렬하기
+-- 역순 정렬하기
 
 SELECT NAME, DATETIME FROM ANIMAL_INS ORDER BY ANIMAL_ID DESC;
 
-### 아픈 동물 찾기
+-- 아픈 동물 찾기
 
 SELECT ANIMAL_ID, NAME 
 FROM ANIMAL_INS
 WHERE INTAKE_CONDITION like 'Sick'
 ORDER BY ANIMAL_ID;
 
-### 어린 동물 찾기
+-- 어린 동물 찾기
 
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
 WHERE INTAKE_CONDITION not like 'Aged'
 ORDER BY ANIMAL_ID;
 
-### 동물의 아이디와 이름
+-- 동물의 아이디와 이름
 
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS;
 
-### 여러 기준으로 정렬하기
+-- 여러 기준으로 정렬하기
 
 SELECT ANIMAL_ID, NAME, DATETIME
 FROM ANIMAL_INS
 ORDER BY NAME, DATETIME desc;
 
-### 상위 N개의 레코드
+-- 상위 N개의 레코드
 
 SELECT NAME
 FROM ANIMAL_INS
